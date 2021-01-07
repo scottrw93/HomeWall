c5cb403f09f43d6e11e6efc3c2cf702aed1e3a3a
scottrw93 <scottrw93@gmail.com>
2021-01-07 21:43:31 +0000

    Cors

--- src/main/java/com/scottw/homewall/HomeWall.java
+++ src/main/java/com/scottw/homewall/HomeWall.java
@@ -4,11 +4,23 @@ import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 import java.io.BufferedWriter;
+import java.net.HttpURLConnection;
 
 public class HomeWall implements HttpFunction {
   @Override
   public void service(HttpRequest request, HttpResponse response) throws Exception {
     BufferedWriter writer = response.getWriter();
+
+    response.appendHeader("Access-Control-Allow-Origin", "*");
+
+    if ("OPTIONS".equals(request.getMethod())) {
+      response.appendHeader("Access-Control-Allow-Methods", "GET");
+      response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
+      response.appendHeader("Access-Control-Max-Age", "3600");
+      response.setStatusCode(HttpURLConnection.HTTP_NO_CONTENT);
+      return;
+    }
+
     writer.write("Hello world!");
   }
 }
